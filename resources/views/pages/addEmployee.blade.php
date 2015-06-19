2dbc431f3dd69253a294e0cfca42a76f741f0de5
Amar <Amar@Amr-PC>
2015-06-19 23:19:34 +0300

    Add Station Add Employee

--- resources/views/pages/addEmployee.blade.php
+++ resources/views/pages/addEmployee.blade.php
@@ -10,10 +10,10 @@
                     <input type="text" class="form-control" name="employee_id" placeholder="Employee ID">
                 </div>
                 <div class="form-group">
-                   <input type="text" class="form-control"name="name" placeholder="Name">
+                    <input type="text" class="form-control"name="name" placeholder="Name">
                 </div>
                 <div class="form-group">
-                     <input type="email" class="form-control" name="email" placeholder="Email">
+                    <input type="email" class="form-control" name="email" placeholder="Email">
                 </div>
                 <div class="form-group">
                     <input type="text" class="form-control" name="phone_1" placeholder="Telephone">
@@ -21,25 +21,25 @@
                 <div class="form-group">
                     <label>Location:</label>
                     <select name="location_id" class="form-control">
-                    @forelse($locations as $location)
-                    <option value="{{ $location->id }}">{{ $location->name }}</option>
-                    @empty
-                    <option>No locations in database</option>
-                    @endforelse
-                </select>
+                        @forelse($locations as $location)
+                        <option value="{{ $location->id }}">{{ $location->name }}</option>
+                        @empty
+                        <option>No locations in database</option>
+                        @endforelse
+                    </select>
                 </div>
                 <div class="form-group">
                     <label>Station:</label>
                     <select name="station_id" class="form-control">
-                    @forelse($stations as $station)
-                    <option value="{{ $station->id }}">{{ $station->name }}</option>
-                    @empty
-                    <option>No Stations in database</option>
-                    @endforelse
-                </select>
+                        @forelse($stations as $station)
+                        <option value="{{ $station->id }}">{{ $station->name }}</option>
+                        @empty
+                        <option>No Stations in database</option>
+                        @endforelse
+                    </select>
                 </div>
                 <div class="form-group">
-                <button type="button" class="btn btn-success"value="submit" name="submit">Submit</button>
+                    <button type="button" class="btn btn-success"value="submit" name="submit">Submit</button>
                 </div>
             </form>  
         </div>
--- resources/views/pages/addStation.blade.php
+++ resources/views/pages/addStation.blade.php
@@ -14,20 +14,20 @@
                     <input type="text" class="form-control" name="address" placeholder="Address">
                 </div>
                 <div class="form-group">
-                     <input type="text"  class="form-control" name="phone_1" placeholder="Telephone">
+                    <input type="text"  class="form-control" name="phone_1" placeholder="Telephone">
                 </div>
                 <div class="form-group">
                     <label>Location</label>
                     <select name="location_id"class="form-control">
-        @forelse($locations as $location)
-        <option value="{{ $location->id }}">{{ $location->name }}</option>
-        @empty
-        <option>No locations in database</option>
-        @endforelse
-    </select>
+                        @forelse($locations as $location)
+                        <option value="{{ $location->id }}">{{ $location->name }}</option>
+                        @empty
+                        <option>No locations in database</option>
+                        @endforelse
+                    </select>
                 </div>
                 <button type="submit" value="submit" name="submit"class="btn btn-success"> Submit</button>
-</form>
+            </form>
         </div>
     </div>
 </div>
