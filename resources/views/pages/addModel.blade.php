3a713b8893b61d2633620091eec86b3fea705d21
Amar <Amar@Amr-PC>
2015-06-20 11:30:15 +0300

    Edit Add Model

--- resources/views/pages/addModel.blade.php
+++ resources/views/pages/addModel.blade.php
@@ -7,13 +7,16 @@
         <div class="col-md-4 col-md-offset-4">
             <h4 style="text-align:center;">Add Model</h4><br/>
             <form class="form-horizontal" name="addModel" method="post" action="{{ url('/models/add') }}">
-                Make:<select name="make_id">
+                <div class="form-group">
+                    <label>Make</label>
+                    <select name="make_id"class="form-control">
                     @forelse($makes as $make)
                     <option value="{{ $make->id }}">{{ $make->name }}</option>
                     @empty
                     <option>No Makes in database</option>
                     @endforelse
-                </select></br>
+                </select>
+                </div>
                 <div class="form-group">
                    <input type="text" class="form-control"name="name" placeholder="Name">
                 </div>
