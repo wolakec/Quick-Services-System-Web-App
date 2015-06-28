5900338b588c9784fbeb915ca89fd8c3fda0983a
Amar <Amar@Amr-PC>
2015-06-28 18:02:13 +0300

    Make Design for menu design for genrate QR code

--- resources/views/pages/addQr.blade.php
+++ resources/views/pages/addQr.blade.php
@@ -1,19 +1,32 @@
 @extends('layouts.default')
 
 @section('content') 
-<h2>Generate QR Codes</h2>
-<form name="qrForm" action="{{ url('/codes/add') }}" method="post">
-    Prefix: <input type="text" name="prefix"/></br>
-    Quota: <select name="quota">
-        <option value="1">1</option>
-        <option value="5">5</option>
-        <option value="10">10</option>
-        <option value="25">25</option>
-        <option value="50">50</option>
-        <option value="100">100</option>
-        <option value="200">200</option>
-        <option value="500">500</option>
-    </select></br>
-    <input type="submit" name="submit" value="Generate"/>
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h4 style="text-align: center;">Generate QR Codes</h4><br/>
+            <form class="form-horizontal" name="qrForm" action="{{ url('/codes/add') }}" method="post">
+                <div class="form-group">
+                    <input type="text" name="prefix" class="form-control" placeholder=" Prefix">
+                </div>
+                <div class="form-group">
+                    <label>Quota</label> 
+                    <select name="quota" class="form-control">
+                        <option value="1">1</option>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                        <option value="200">200</option>
+                        <option value="500">500</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="submit" value="Generate"class="btn btn-success">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @stop
---
+++ resources/views/pages/summary.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.default')
+
+@section('content')   
+
+<div class="container outer-section">
+    <div id="print-area">
+        <div class="row pad-top font-big">
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                <img src="{{ asset('Images/logo.png') }}" width="200" height="75" alt="Petromin Logo" />
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                <strong>Support : </strong>info@petromin.com
+                <br />
+                <strong>Tel :</strong>+249-183-560-592<br />
+                <strong>Fax :</strong>+249-183-560-550<br />
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                PETROMIN CORPORATION – SUDAN BRANCH
+                Khartoum (3) – Street No. 39<br/>
+                P.O. Box 15064
+                <br />
+                Khartoum-Sudan.<br />
+            </div>
+        </div>
+        <br />
+        <hr />
+        <div class="row text-center">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                Account Created successfully , for any issues please contact &nbsp;<strong>info@petromin.com</strong>
+            </div>
+        </div>
+        <hr/>
+        <div class="row ">
+            <div class="col-lg-6 col-md-6 col-sm-6">              
+                <h4>Personal Information</h4>
+                <strong>Name :</strong>    <br/>
+                <strong>Tel :</strong>    <br/>
+                <strong>Address:</strong>    <br/>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <h4>Login Details:</h4>
+                <strong> Email:</strong> {{ $email }} <br/>
+                <strong> Password:</strong> {{ $password }} <br/>
+            </div>
+        </div>
+        <hr>
+        <div class="row pad-bottom">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <a href="#" class="btn btn-success " style="width: 80px;float: right;margin-right: 100px;"> Print </a>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
