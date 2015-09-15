8679ecc01c6a3081b6407bbe0bb0585a4ee22ba5
Work station <Work station@Mactavish>
2015-09-15 20:12:04 +0300

    styled the charts

--- resources/views/pages/viewMakeStatistics.blade.php
+++ resources/views/pages/viewMakeStatistics.blade.php
@@ -4,10 +4,10 @@
 <h3 style="text-align: center;">Vehicle Make Statistics</h3><br>
         <div class="container">
             <div class="row">
-                <div id='stocks-div' class="col-md-12">
-                   
+                <div class='chart-con-div' class="col-lg-offset-3 col-md-12">
+                   {!! Lava::render('PieChart', 'Makes', 'chart-div',array('height'=>500, 'width'=>800)) !!}
+
                 </div>
             </div>
         </div>
-{!! Lava::render('PieChart', 'Makes', 'test-div',array('height'=>500, 'width'=>800)) !!}
 @stop
