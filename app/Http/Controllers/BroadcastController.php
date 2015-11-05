dacda2cec1593eed37a80443c5c34c209efaac66
Ak3c <wolakec@live.com>
2015-11-05 07:09:42 +0300

    Broadcast now using google cloud messaging

--- app/Http/Controllers/BroadcastController.php
+++ app/Http/Controllers/BroadcastController.php
@@ -4,6 +4,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\notificationRequest;
 use Illuminate\Http\Request;
+use PushNotification;
 use App\Client;
 use App\Notification;
 use App\BroadcastHistory;
@@ -26,15 +27,27 @@ class BroadcastController extends Controller {
     public function store(notificationRequest $request)
     {
        $clients = Client::all();
+       $deviceList = array();
        
-       foreach($clients as $client){
+        foreach($clients as $client){
           
-           $notification = Notification::create($request->all());
-           $notification->client_id = $client->id;
-           $notification->status = "pending";
-           $notification->type = "broadcast";
-           $notification->save();
+            if($client->gcm_token){
+                $deviceList[] = PushNotification::device($client->gcm_token);
+            }
        }
+       
+       $devices = PushNotification::deviceCollection($deviceList);
+       
+       $message = PushNotification::Message($request->message,
+                     array('title' => $request->title,
+                         'type' => 'reminder',
+                         )
+                     );
+
+                 $push = PushNotification::app('qssClient')
+                         ->to($devices)
+                         ->send($message);
+       
        
        $broadcastHistory = BroadcastHistory::create($request->all());
        $broadcastHistory->user_id = $request->user()->id;
